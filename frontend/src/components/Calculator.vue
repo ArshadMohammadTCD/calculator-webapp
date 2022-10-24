03a4792383c6c44ce2f2d9b7af31c6904362ef13
WesLemon <wesleyshaw25@gmail.com>
2022-10-24 13:41:10 +0100

    frontend fully initialised

---
+++ frontend/src/components/Calculator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hello world!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalculatorApp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
