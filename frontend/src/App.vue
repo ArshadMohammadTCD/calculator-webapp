c54585f20c65714d6ff57f5523d6e7628a2e651e
WesLemon <wesleyshaw25@gmail.com>
2022-10-24 16:55:25 +0100

    Formatted code, added an icon, a title & a footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <CalculatorApp></CalculatorApp>
+    <AppFooter></AppFooter>>
   </div>
+
 </template>
 
 <script>
 import CalculatorApp from './components/Calculator.vue'
+import AppFooter from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    CalculatorApp
+    CalculatorApp,
+    AppFooter
   }
 }
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <MDBFooter :text="['center', 'lg-start']">
+
+        <div class="fixed-bottom text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            <a class="text-dark">
+                Developed by Wesley Shaw, Abigail Amethyst, Paddi Hua and Arshad Mohammed for CSU33012
+            </a>
+        </div>
+        <!-- Copyright -->
+    </MDBFooter>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
