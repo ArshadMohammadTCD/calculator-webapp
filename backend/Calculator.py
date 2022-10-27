a92e99247f3a2cc7419a9498b62a53dd5658944d
Paddi Hua <phua@tcd.ie>
2022-10-27 14:27:41 +0100

    main calc done

--- backend/Calculator.py
+++ backend/Calculator.py
@@ -1,88 +1,163 @@
 from cmath import sqrt
 import math
 
-
 def calc(eq):
-    op = []
-    num = []
+    op = []      #operator list
+    num = []     #number list
 
     i = 0
-    j = 0
     while i < len(eq):
         if eq[i] == "+" or eq[i] == "-" or eq[i] == "*" or eq[i] == "/" or eq[i] == "^" or eq[i] == "log" or eq[i] == "exp":
-            op.append(eq[i])
+            if eq[i+1] == "log" and i == 0:                            #check if -log at the start
+                neg = math.log10(float(eq[i+2])) * -1
+                num.append(neg)
+                i += 2
+            elif eq[i+1] == "exp" and i == 0:                          #check if -exp at the start
+                neg = math.exp(float(eq[i+2])) * -1
+                num.append(neg)
+                i += 2
+            elif eq[i+1] == "-":                                       #check is there neg numbers in the equation
+                if eq[i+2] == "log":                                   #check anything like 1 + -log(1) where i should be at "+"
+                    neg = math.log10(float(eq[i+3])) * -1
+                    num.append(neg)
+                    op.append(eq[i])
+                    i += 3
+                elif eq[i+2] == "exp":
+                    neg = math.exp(float(eq[i+3])) * -1                #check anything like 1 + -exp(1) where i should be at "+"
+                    num.append(neg)
+                    op.append(eq[i])
+                    i += 3
+                else:
+                    neg = float(eq[i+2]) * -1                          #turn to neg numbers and store into the list
+                    num.append(neg)
+                    op.append(eq[i])
+                    i += 2
+            elif eq[i] == "-" and i == 0:                              #check is there "-" at the start, e.g.-2+1
+                neg = float(eq[i+1]) * -1
+                num.append(neg)
+                i += 1
+            else:
+                op.append(eq[i])                                       #if no neg in case, just add to op list
         else:
-            num.append(eq[i])
-            j += 1
+            num.append(eq[i])                                          #if not operator, then has to be a number(with error checking)
         i += 1
+
     count = 0
     result = 0
+
     while count < len(op):
-        if op[count] == "^":
-            ans = float(num[count]) ** float(num[count+1])
+        if op[count] == "^":                                           #"^"should be the first to implement when we have bracket outside
+            ans = float(num[count]) ** float(num[count+1])             #calculate answer
+            num[count+1] = ans                                         #store into list
+            del num[count]                                             #delete the other used number in the list
+            del op[count]                                              #delete the used operator
+        else:
+            count += 1
+    
+    count = 0                                                          #set count back to 0 to enter another while
+
+    while count < len(op):                                             #check and calculate log and exp before anything else
+        if op[count] == "log":
+            ans = math.log10(float(num[count]))                        #nature log with a base of 10
+            num[count] = ans
+            del op[count]
+        elif op[count] == "exp":
+            ans = math.exp(float(num[count]))                          #calculate exp
+            num[count] = ans
+            del op[count]
+        count += 1
+    
+    count = 0
+
+    while count < len(op):
+        if op[count] == "*":
+            ans = float(num[count]) * float(num[count+1])              #calculate mul
             num[count+1] = ans
             del num[count]
             del op[count]
-        elif op[count] == "*":
-            ans = float(num[count]) * float(num[count+1])
-            num[count + 1] = ans
-            del num[count]
-            del op[count]
         elif op[count] == "/":
-            ans = float(num[count]) / float(num[count+1])
-            num[count + 1] = ans
+            ans = float(num[count]) / float(num[count+1])              #calculate div
+            num[count+1] = ans
             del num[count]
             del op[count]
         else:
             count += 1
-    result += float(num[0])
-    for x in range(0, len(op)):
+    result += float(num[0])                                            #set result to the first value in number list
+    for x in range(0, len(op)):                                        #loop through and calculate the rest "+" and "-" operator
         if op[x] == "+":
             result += float(num[x+1])
         elif op[x] == "-":
             result -= float(num[x+1])
-    return result
+    return result                                                      #return final result
 
-
-equation = "2.01^3*2/4"
-temp = ""
-eq = []
-for c in range(0, len(equation)):
-    if equation[c] == "+" or equation[c] == "-" or equation[c] == "*" or equation[c] == "/" or equation[c] == "^" or equation[c] == "log" or equation[c] == "exp":
+equation = "1+(2-1)+(7+2)-5"                                                    #only use for debug
+temp = ""                                                              #temp string to load space
+eq = []                                                                #list of main equation
+index = 0
+while index < len(equation):                                           #if operator, add space to front and end, e.g.1+1 turn into 1 + 1
+    if equation[index] == "+" or equation[index] == "*" or equation[index] == "/" or equation[index] == "^":
+        if equation[index+1] == "-":                                   #if next is also an aperator(negative op), only add space at front
+            temp += " "
+            temp += equation[index]
+        else:                                                          #other operator
+            temp += " "
+            temp += equation[index]
+            temp += " "
+    elif equation[index] == "-":                                       #3 case when "-"
+        if index == 0:                                                 #if start with neg element, only add space to the end
+            temp += equation[index]
+            temp += " "
+        elif equation[index+1] == "-":                                 #check anything like 1--1
+            temp += " "
+            temp += equation[index]
+        else:                                                          #normal "-" operator
+            temp += " "
+            temp += equation[index]
+            temp += " "
+    elif equation[index] == "l":                                       #in string you get l as the first character
+        temp += "log"                                                  #set the whole word log into the list
         temp += " "
-        temp += equation[c]
+        index += 2                                                     #skip character "o" and "g"
+    elif equation[index] == "e":                                       #e for exp
+        temp += "exp"
         temp += " "
-    elif equation[c] == "(":
-        temp += equation[c]
+        index += 2
+    elif  equation[index] == "(":                                      #open bracket operator
+        if equation[index+1] == "-":                                   #check anything like 1+(-1+1)
+            temp += equation[index]
+        else:                                                          #normal open bracket
+            temp += equation[index]
+            temp += " "
+    elif equation [index] == ")":                                      #close bracket operator
         temp += " "
-    elif equation[c] == ")":
-        temp += " "
-        temp += equation[c]
-    else:
-        temp += equation[c]
+        temp += equation[index]
+    else:                                                              #when is a number
+        temp += equation[index]
+    index += 1
 eq = list(temp.split(" "))
 
 i = 0
 j = 0
 k = 0
-bracket = []
+bracket = []                                                           #list to calculate bracket
 result = 0
 while i < len(eq):
-    if eq[i] == "(":
-        j = i + 1
-        k = i
-        while eq[j] != ")":
+    if eq[i] == "(":                                                   #if there is a bracket
+        j = i + 1                                                      #pointer for the equation in the bracket
+        k = i                                                          #temp pointer for i
+        while eq[j] != ")":                                            #add equation to the list until find a close bracket
             bracket.append(eq[j])
             j += 1
-        result = calc(bracket)
-        while k <= j:
-            del eq[i]
+        result = calc(bracket)                                         #calculate the result
+        while k <= j:                                                  #check is the temp pointer reach the end of the bracket
+            del eq[i]                                                  #delete the whole bracket
             k += 1
-        eq.insert(i, str(result))
+        eq.insert(i, str(result))                                      #insert the answer into the place of bracket before
+        bracket.clear();                                               #clear the bracket for next time
     else:
         i += 1
-result = calc(eq)
-print("%.3f" % result)
+result = calc(eq)                                                      #get the whole result
+print("%.3f" % result)                                                 #print it with 3 digit
 
 
 # errorCheck
